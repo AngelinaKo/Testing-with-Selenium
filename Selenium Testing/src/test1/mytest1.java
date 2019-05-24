1737754c03134caf94e0611947f509f506d82516
user <user@users-mbp.home>
2019-05-24 15:40:54 +0200

    Test example 1

---
+++ Selenium Testing/src/test1/mytest1.java
@@ -0,0 +1,27 @@
+package test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class mytest1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/Users/user/Documents/selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.toastmasters.org/");
+		driver.findElement(By.id("txtSiteSearch")).sendKeys("Pathways");
+		driver.findElement(By.id("btnSiteSearch")).click();
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Toastmasters International -";
+		driver.close();
+		if(actualTitle.contentEquals(expectedTitle))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+	}
+
+}
